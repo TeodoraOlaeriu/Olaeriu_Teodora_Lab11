20964f0df3f0afdfba415b3ffa6df9d7a8adf8b0
Teo <teodoraolaeriu@yahoo.com>
2022-12-20 15:53:46 +0200

    Add project files.

---
+++ Olaeriu_Teodora_Lab11/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Olaeriu_Teodora_Lab11;
+using Olaeriu_Teodora_Lab11.Data;
+
+namespace Olaeriu_Teodora_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Olaeriu_Teodora_Lab11/ListPage.xaml.cs
@@ -0,0 +1,21 @@
+using Olaeriu_Teodora_Lab11;
+using Olaeriu_Teodora_Lab11.Models;
+
+namespace Olaeriu_Teodora_Lab11;
+
+public partial class ListPage : ContentPage
+{
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+}
